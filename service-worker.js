9a4f54181e596e917c695f0e8e28594d50de02ae
Dima Batiuk <batiuk20@gmail.com>
2021-05-25 14:04:16 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.0cb9625b44086f305c0a9873f7bdda5e.js"
 );
 
 self.addEventListener('message', (event) => {
